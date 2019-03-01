272b82ac53f1efa03ab978bacf8071acab040f70
Marius Guščius <masterj2@gmail.com>
2019-03-01 22:25:57 +0200

    fixed eslint issues

--- src/components/app/app.component.vue
+++ src/components/app/app.component.vue
@@ -4,74 +4,8 @@
 </template>
 
 <script>
-    export default {
-        // beforeCreate()
-        // {
-        //     menu.create({
-        //         File: {
-        //             Refresh: {
-        //                 modifiers: "ctrl",
-        //                 key: "r",
-        //                 click: () =>
-        //                 {
-        //                     this.refreshData();
-        //                 }
-        //             },
-        //             separator1: null,
-        //             Exit: {
-        //                 key: "esc",
-        //                 click: function()
-        //                 {
-        //                     nw.App.quit();
-        //                 }
-        //             }
-        //         }
-        //     });
-        //     this.$on("disable-menu", function(isDisabled = false) {
-        //         isDisabled ? menu.disable() : menu.enable();
-        //     });
-        // },
-        // mounted()
-        // {
-        //     this.$nextTick(async function() {
-        //         if (await isInitialized() === false) {
-        //             await this.refreshData();
-        //             await setInitialized();
-        //         }
-        //     });
-        // },
-        // beforeDestroy()
-        // {
-        //     this.$off();
-        // },
-        // components: {
-        //     AppMain,
-        //     AppFooter,
-        //     AppSide,
-        //     Downloader
-        // },
-        // data() {
-        //     return {
-        //         selectedCountryId: null,
-        //     };
-        // },
-        // methods: {
-        //     async refreshData()
-        //     {
-        //         await this.$refs.Downloader.init();
-        //         this.refreshSide();
-        //         this.changeCountry(null);
-        //     },
-        //     changeCountry(countryId)
-        //     {
-        //         this.selectedCountryId = countryId;
-        //     },
-        //     refreshSide()
-        //     {
-        //         this.$refs.AppSide.refreshCountryList();
-        //     }
-        // }
-    };
+  export default {
+  };
 </script>
 
 <style src="vuetify/dist/vuetify.min.css"></style>
