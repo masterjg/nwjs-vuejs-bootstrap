5d74688bc38cdc48e51e8754ce0224e344d825db
Marius Guščius <masterj2@gmail.com>
2019-03-09 23:24:46 +0200

    added missing v-layout

--- src/components/main/main.component.vue
+++ src/components/main/main.component.vue
@@ -1,16 +1,18 @@
 <template>
-<v-flex
-  id="content"
-  fill-height
-  pa-2
->
-  <v-toolbar-title class="grey--text text--darken-4">
-    Computer info
-  </v-toolbar-title>
-  <CPUInfo />
-  <MemoryInfo />
-  <StorageInfo />
-</v-flex>
+<v-layout>
+  <v-flex
+    id="content"
+    fill-height
+    pa-2
+  >
+    <v-toolbar-title class="grey--text text--darken-4">
+      Computer info
+    </v-toolbar-title>
+    <CPUInfo />
+    <MemoryInfo />
+    <StorageInfo />
+  </v-flex>
+</v-layout>
 </template>
 
 <script>
@@ -31,5 +33,9 @@ export default {
 #content {
   overflow-y: scroll;
   position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
